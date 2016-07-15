99aa71e143047834f80dd5da893523803a27ed24
Roman Kurilko <kurilkor@gmail.com>
2016-07-15 18:44:01 +0300

    test

--- module2/src/main/java/com/courses/spalah/Application.java
+++ module2/src/main/java/com/courses/spalah/Application.java
@@ -19,7 +19,9 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        Map map = new HashMap<>();
+
+        //Map map = new HashMap<>();
+
     }
 
     public String getName() {
