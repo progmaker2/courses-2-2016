a123c1055983d3f63c24565cd1f13c905a61cd39
Ievgen Tararaka <ievgentararaka@gmail.com>
2016-09-30 17:24:54 +0300

    Add module 20

---
+++ module20/src/main/java/com/courses/spalah/Person.java
@@ -0,0 +1,25 @@
+package com.courses.spalah;
+
+/**
+ * @author Ievgen Tararaka
+ */
+public class Person {
+    public String firstName;
+    public int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
